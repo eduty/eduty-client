f911dee265cd93c6b9debc56a90549ae8b5c7b59
Vinícius Hoyer <vhoyer@live.com>
2019-06-01 18:49:41 -0300

    Add media youtube component

---
+++ src/components/media/media-youtube.vue
@@ -0,0 +1,18 @@
+<template>
+  <iframe
+    src="`https://www.youtube.com/embed/${videoId}`"
+    frameborder="0"
+  />
+</template>
+
+<script>
+export default {
+  name: 'MediaYoutube',
+  props: {
+    videoId: {
+      required: true,
+      type: String,
+    },
+  },
+}
+</script>
