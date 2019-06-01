bceffc4b97b2245c23d38d4caddb0e6950e9a112
Vinícius Hoyer <vhoyer@live.com>
2019-06-01 19:49:50 -0300

    Restructure user-page

--- src/pages/user/profile.vue
+++ src/pages/user/profile.vue
@@ -2,19 +2,28 @@
   <v-layout>
     <v-flex text-xs-center>
       <UserAvatar :size="200" />
+      <h1>{{ user.fullName }}</h1>
       <p>
         {{ $route.params.userId }}
       </p>
+
+      oi esse é meu projeto
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import { UserAvatar } from '~/components/user'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     UserAvatar,
+  },
+  computed: {
+    ...mapState('user', [
+      'user',
+    ]),
   },
 }
 </script>
