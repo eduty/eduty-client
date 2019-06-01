ee4892ff18fb71be98c4df64a8bfd65c3dc58fbb
Vinícius Hoyer <vhoyer@live.com>
2019-06-01 19:14:02 -0300

    add user avatar component

---
+++ src/components/user/user-avatar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-avatar
+    :tile="tile"
+    :size="avatarSize"
+    color="grey lighten-4"
+  >
+    <img
+      :src="`https://www.gravatar.com/avatar/${emailHash}?s=${size}`"
+      :height="size"
+      :width="size"
+      alt="avatar"
+    >
+  </v-avatar>
+</template>
+
+<script>
+import md5 from 'md5'
+import { mapState } from 'vuex'
+
+// https://en.gravatar.com/site/implement/images/
+// https://en.gravatar.com/site/implement/hash/
+export default {
+  name: 'UserAvatar',
+  computed: {
+    ...mapState('user', [
+      'user',
+    ]),
+    emailHash() {
+      return md5(this.user.email)
+    },
+  },
+}
+</script>
