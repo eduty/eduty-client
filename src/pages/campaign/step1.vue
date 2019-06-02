35083afe86562ba20b876211a237c74ee09dc50f
Heitor Miranda <heitor-mm@hotmail.com>
2019-06-02 05:08:37 -0300

    create user integration

--- src/pages/campaign/step1.vue
+++ src/pages/campaign/step1.vue
@@ -28,7 +28,7 @@
               <v-text-field
                 v-model="name"
                 label="Qual o seu nome?"
-                required
+                :rules="[rules.required]"
               />
 
               <v-layout
@@ -43,7 +43,6 @@
                     v-model="email"
                     label="Qual o seu e-mail"
                     type="email"
-                    required
                     :rules="emailRules"
                   />
                 </v-flex>
@@ -56,7 +55,7 @@
                     v-model="phone"
                     label="Qual o seu telefone?"
                     type="tel"
-                    required
+                    :rules="[rules.required]"
                   />
                 </v-flex>
 
@@ -67,7 +66,7 @@
                   <v-text-field
                     v-model="city"
                     label="Em qual cidade mora?"
-                    required
+                    :rules="[rules.required]"
                   />
                 </v-flex>
 
@@ -78,7 +77,7 @@
                   <v-text-field
                     v-model="state"
                     label="Qual estado?"
-                    required
+                    :rules="[rules.required]"
                   />
                 </v-flex>
 
@@ -91,7 +90,6 @@
                     label="Digite uma senha"
                     type="password"
                     min="8"
-                    required
                     :rules="passwordRules"
                   />
                 </v-flex>
@@ -105,7 +103,6 @@
                     label="Confirmar sua senha"
                     type="password"
                     min="8"
-                    required
                     :rules="passwordRules"
                   />
                 </v-flex>
@@ -113,7 +110,7 @@
 
               <e-button
                 class="step__action mt-3"
-                type="primary"
+                @click="submit"
               >
                 Próximo passo
               </e-button>
@@ -126,11 +123,53 @@
 </template>
 
 <script>
+import axios from 'axios'
 import EButton from '~/components/ui/e-button'
 
 export default {
   components: {
     EButton,
+  },
+  data() {
+    return {
+      city: '',
+      confirmPassword: '',
+      email: '',
+      emailRules: [
+        v => !!v || 'Campo obrigatório',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail deve ser válido',
+      ],
+      name: '',
+      password: '',
+      passwordRules: [
+        v => !!v || 'Campo obrigatório',
+      ],
+      phone: '',
+      rules: {
+        required: value => !!value || 'Campo obrigatório.',
+      },
+      state: '',
+      valid: false,
+    }
+  },
+  methods: {
+    nextStep() {
+      this.$router.push('/campanha/sonho')
+    },
+    submit() {
+      if (this.$refs.form.validate() && this.password === this.confirmPassword) {
+        axios.post('/api/users', {
+          city: this.city,
+          email: this.email,
+          name: this.name,
+          password: this.password,
+          phone: this.phone,
+          state: this.state,
+        }).then(() => {
+          this.nextStep()
+        })
+      }
+    },
   },
 }
 </script>
