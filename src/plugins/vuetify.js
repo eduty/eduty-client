c4dc0347143dcf0e3c45e77c919334a3e9843048
Vinícius Hoyer <vhoyer@live.com>
2019-06-01 23:20:29 -0300

    Add font awesome icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import colors from 'vuetify/es5/util/colors'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+library.add(fas, fab) // Include needed icons.
 
 Vue.use(Vuetify, {
+  iconfont: 'faSvg',
   theme: {
     primary: '#1C2D4D',
     secondary: '#46546E',
