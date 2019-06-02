45b623050ba751af67bb23a2813faa2adca63739
Marcelo Fraga <marcelo.fraga@me.com>
2019-06-01 21:49:03 -0300

    Add header component

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,41 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
+    <Header />
 
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-toolbar
-      app
-      flat
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-
-      <nuxt-link to="/">
-        <v-toolbar-title v-text="title" />
-      </nuxt-link>
-    </v-toolbar>
-
-    <v-content>
+    <v-content style="padding-top: 80px;">
       <v-container>
         <nuxt />
       </v-container>
@@ -54,19 +21,11 @@
 </template>
 
 <script>
+import { Header } from '~/components/layout'
+
 export default {
-  data() {
-    return {
-      drawer: false,
-      items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/',
-        },
-      ],
-      title: 'Eduty',
-    }
+  components: {
+    Header,
   },
 }
 </script>
