8e2cbf97b3d1942dec3ab97f3312e60664c1b5cd
Vinícius Hoyer <vhoyer@live.com>
2019-06-01 21:17:06 -0300

    add content to progress

---
+++ src/components/funding/funding-progress-bar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="progress-background">
+    100%
+    <div
+      v-for="(installment, index) in installments"
+      :key="index"
+      :class="{ 'installment--paid': installment.paid }"
+      class="installment"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FundingProgressBar',
+  props: {
+    installments: {
+      required: true,
+      type: Array,
+      validator: array => array.every(item => [
+        'paid',
+      ].every(key => item.hasOwnProperty(key))),
+    },
+  },
+}
+</script>
+
+<style src="./funding-progress-bar.css" scoped>
